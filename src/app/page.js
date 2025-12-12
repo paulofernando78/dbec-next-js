fabeccb17041cd20af4db036301ba4864313360b
Paulo Silva <paulofernando78@gmail.com>
2025-12-12 15:51:11 -0300

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,93 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <h2>Home Page</h2>
-      <h3>Home Page</h3>
-      <h4>Home Page</h4>
-      <h5>Home Page</h5>
-      <h6>Home Page</h6>
-      <div className="line-break">
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
-        quis quisquam ipsum qui, minus odio quam vero sunt harum cumque optio
-        pariatur sint, ducimus ullam delectus omnis aliquid possimus!
-        Perferendis.
-      </p>
+    <div className="line-break">
+      <div>
+        <h1>Home Page</h1>
+        <h2>Home Page</h2>
+        <h3>Home Page</h3>
+        <h4>Home Page</h4>
+        <div className="line-break">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequuntur quis quisquam ipsum qui, minus odio quam vero sunt
+            harum cumque optio pariatur sint, ducimus ullam delectus omnis
+            aliquid possimus! Perferendis.
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Consequuntur quis quisquam ipsum qui, minus odio quam vero sunt
+            harum cumque optio pariatur sint, ducimus ullam delectus omnis
+            aliquid possimus! Perferendis.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h1>Home Page</h1>
+        <div className="line-break">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+            quis quisquam ipsum qui, minus odio quam vero sunt harum cumque optio
+            pariatur sint, ducimus ullam delectus omnis aliquid possimus!
+            Perferendis.
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+            quis quisquam ipsum qui, minus odio quam vero sunt harum cumque optio
+            pariatur sint, ducimus ullam delectus omnis aliquid possimus!
+            Perferendis.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2>Home Page</h2>
+        <div className="line-break">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+            quis quisquam ipsum qui, minus odio quam vero sunt harum cumque optio
+            pariatur sint, ducimus ullam delectus omnis aliquid possimus!
+            Perferendis.
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+            quis quisquam ipsum qui, minus odio quam vero sunt harum cumque optio
+            pariatur sint, ducimus ullam delectus omnis aliquid possimus!
+            Perferendis.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h3>Home Page</h3>
+        <div className="line-break">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+            quis quisquam ipsum qui, minus odio quam vero sunt harum cumque optio
+            pariatur sint, ducimus ullam delectus omnis aliquid possimus!
+            Perferendis.
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+            quis quisquam ipsum qui, minus odio quam vero sunt harum cumque optio
+            pariatur sint, ducimus ullam delectus omnis aliquid possimus!
+            Perferendis.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h4>Home Page</h4>
+        <div className="line-break">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+            quis quisquam ipsum qui, minus odio quam vero sunt harum cumque optio
+            pariatur sint, ducimus ullam delectus omnis aliquid possimus!
+            Perferendis.
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+            quis quisquam ipsum qui, minus odio quam vero sunt harum cumque optio
+            pariatur sint, ducimus ullam delectus omnis aliquid possimus!
+            Perferendis.
+          </p>
+        </div>
       </div>
     </div>
   );
