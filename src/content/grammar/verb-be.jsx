280c1bc5fcf3ed1485cc67aabd7416449ab4ed71
Paulo Silva <paulofernando78@gmail.com>
2025-12-16 02:49:26 -0300

    wip

--- src/content/grammar/verb-be.jsx
+++ src/content/grammar/verb-be.jsx
@@ -20,13 +20,13 @@ export default function VerbBe() {
         <Text playAudio="/audio/grammar/verb-be/identidade/theyre-my-friends.mp3"><BulletPoint /> They are my friends. <span className="portuguese">Eles são meus amigos.</span></Text>
         </div> 
         <div>
-        <Text playAudio=""><BulletPoint /> Profissão:</Text>
+        <Text playAudio="">Profissão:</Text>
         <Text playAudio=""><BulletPoint /> I’m a teacher.</Text>
         <Text playAudio=""><BulletPoint /> He’s an enginner.</Text>
         <Text playAudio=""><BulletPoint /> She’s a doctor.</Text>
         </div>
         <div>
-        <Text playAudio=""><BulletPoint /> Estado / Condição:</Text>
+        <Text playAudio="">Estado / Condição:</Text>
         <Text playAudio=""><BulletPoint /> I’m tired.</Text>
         <Text playAudio=""><BulletPoint /> She’s happy.</Text>
         <Text playAudio=""><BulletPoint /> They’re ready to go.</Text>
@@ -39,24 +39,24 @@ export default function VerbBe() {
         </div>
         <Ribbon label="Affirmative"/>
         <div>
-          <Text> I <span className="bold">am</span> Paulo. My name <span className="bold">is</span> Paulo.</Text>
-          <Text> I <span className="bold">am</span> the teacher. You are the student.</Text>
-          <Text>He <span className="bold">is</span> Mike. He is a doctor.</Text>
-          <Text>She <span className="bold">is</span> a photographer.</Text>
-          <Text>It <span className="bold">is</span> cat. Her name is Cora.</Text>
-          <Text>We <span className="bold">are</span> Brazilian.</Text>
-          <Text>You <span className="bold">are</span> students.</Text>
-          <Text>They <span className="bold">are</span> teachers.</Text>
+          <Text> I <span className="bold">am</span></Text>
+          <Text>You are</Text>
+          <Text>He <span className="bold">is</span></Text>
+          <Text>She <span className="bold">is</span></Text>
+          <Text>It <span className="bold">is</span></Text>
+          <Text>We <span className="bold">are</span></Text>
+          <Text>You <span className="bold">are</span></Text>
+          <Text>They <span className="bold">are</span></Text>
         </div>
         <Ribbon label="Negative"/>
         <div>
-          <Text> I <span className="bold">am not</span> a student. My name <span className="bold">is not</span> John.</Text>
-          <Text> You <span className="bold">are not</span> the teacher.</Text>
-          <Text>Mike <span className="bold">is not</span> teacher.</Text>
-          <Text>She <span className="bold">is</span> a photographer.</Text>
-          <Text>It <span className="bold">is</span> cat. Her name is Cora.</Text>
-          <Text>You <span className="bold">are</span> students.</Text>
-          <Text>They <span className="bold">are</span> teachers.</Text>
+          <Text> I <span className="bold">am not</span></Text>
+          <Text> You <span className="bold">are not</span></Text>
+          <Text>Mike <span className="bold">is not</span></Text>
+          <Text>She <span className="bold">is not</span></Text>
+          <Text>It <span className="bold">is not</span></Text>
+          <Text>You <span className="bold">are not</span></Text>
+          <Text>They <span className="bold">are not</span></Text>
         </div>
       </div>
       
