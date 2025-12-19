78b4bb054749c6073a04319fbfa94e52e45ba730
Paulo Silva <paulofernando78@gmail.com>
2025-12-19 03:11:29 -0300

    wip

--- src/app/students/extras/selaa/[slug]/page.jsx
+++ src/app/students/extras/selaa/[slug]/page.jsx
@@ -1,36 +1,36 @@
-import fs from "fs";
-import path from "path";
-import { notFound } from "next/navigation";
+// import fs from "fs";
+// import path from "path";
+// import { notFound } from "next/navigation";
 
-export default async function PronunciationPage( {params} ) {
-  const { slug } = await params;
+// export default async function PronunciationPage( {params} ) {
+//   const { slug } = await params;
 
-  try {
-    const Content = (
-      await import(`@/content/students/extras/selaa/${slug}.jsx`)
-    ).default;
+//   try {
+//     const Content = (
+//       await import(`@/content/students/extras/selaa/${slug}.jsx`)
+//     ).default;
 
-    return (
-      <section>
-        <Content />
-      </section>
-    )
+//     return (
+//       <section>
+//         <Content />
+//       </section>
+//     )
 
-  } catch (error) {
-    notFound()
-  }
-}
+//   } catch (error) {
+//     notFound()
+//   }
+// }
 
-export function generateStaticParams() {
-  const dir = path.join(
-    process.cwd(),
-    "src/content/students/extras/selaa"
-  );
+// export function generateStaticParams() {
+//   const dir = path.join(
+//     process.cwd(),
+//     "src/content/students/extras/selaa"
+//   );
 
-  return fs
-    .readdirSync(dir)
-    .filter(file => file.endsWith(".jsx"))
-    .map(file => ({
-      slug: file.replace(".jsx", ""),
-    }));
-}
+//   return fs
+//     .readdirSync(dir)
+//     .filter(file => file.endsWith(".jsx"))
+//     .map(file => ({
+//       slug: file.replace(".jsx", ""),
+//     }));
+// }
