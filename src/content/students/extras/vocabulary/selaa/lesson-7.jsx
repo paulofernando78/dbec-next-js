59e82a69313a0e840800ef51417553eecf4c7b3a
Paulo Silva <paulofernando78@gmail.com>
2025-12-26 23:19:20 -0300

    wip

--- src/content/students/extras/vocabulary/selaa/lesson-7.jsx
+++ src/content/students/extras/vocabulary/selaa/lesson-7.jsx
@@ -3,7 +3,6 @@ import { Card } from "@/components/atoms/Card";
 import { AudioPlayer } from "@/components/atoms/AudioPlayer";
 import { Text } from "@/components/molecules/Text";
 
-
 export default function SELAAlessonOne() {
   return (
     <>
@@ -14,38 +13,40 @@ export default function SELAAlessonOne() {
         lesson="Lesson 7 • Susan Hires Bob to Run Her Business"
       />
       <div className="line-break">
-        <AudioPlayer src="/audio/vocabulary/selaa/lesson-7.mp3"/>
+        <AudioPlayer src="/audio/vocabulary/selaa/lesson-7.mp3" />
         <Card>
-          Susan stays up all night thinking about her
-cookie business. In the morning, she discusses it
-with Bob. Bob agrees to work for her.
+          Susan stays up all night thinking about her cookie business. In the
+          morning, she discusses it with Bob. Bob agrees to work for her.
         </Card>
         <Text>
-          <strong>Bob:</strong> You’re up bright and early this morning, Susan.
+          <strong>Bob:</strong> You’re up <mark>bright and early</mark> this
+          morning, Susan.
         </Text>
         <Text>
-          <strong>Susan:</strong> I didn’t sleep a wink. I was awake all night thinking
-about the new business.
+          <strong>Susan:</strong> I <mark>didn’t sleep a wink</mark>. I was
+          awake all night thinking about the new business.
         </Text>
         <Text>
-          <strong>Bob:</strong> Running your own business is lots of work. Are you
-prepared to work like a dog?
+          <strong>Bob:</strong> Running your own business is lots of work. Are
+          you prepared to <mark>work like a dog</mark>?
         </Text>
         <Text>
-          <strong>Susan:</strong> No. But I am prepared to hire you to run the business.
+          <strong>Susan:</strong> No. But I am prepared to hire you to run the
+          business.
         </Text>
         <Text>
-          <strong>Bob:</strong> You want me to run a cookie business? Fat chance!
+          <strong>Bob:</strong> You want me to run a cookie business?{" "}
+          <mark>Fat chance!</mark>
         </Text>
         <Text>
           <strong>Susan:</strong> Why not?
         </Text>
         <Text>
-          <strong>Bob:</strong> I don’t have a clue about making cookies. I don’t even
-know how to turn the oven on!
+          <strong>Bob:</strong> <mark>I don’t have a clue</mark> about making
+          cookies. I don’t even know how to turn the oven on!
         </Text>
         <Text>
-          <strong>Susan:</strong> I’ll give you a crash course.
+          <strong>Susan:</strong> I’ll give you a <mark>crash course</mark>.
         </Text>
         <Text>
           <strong>Bob:</strong> Do I have to do the baking?
@@ -54,15 +55,16 @@ know how to turn the oven on!
           <strong>Susan:</strong> No. You’ll just manage the business side.
         </Text>
         <Text>
-          <strong>Bob:</strong> Needless to say, I have mixed feelings about working
-for you.
+          <strong>Bob:</strong> <mark>Needless to say</mark>, I have{" "}
+          <mark>mixed feelings</mark> about working for you.
         </Text>
         <Text>
-          <strong>Susan:</strong> I’ll be nice. I promise you’ll be a happy camper.
+          <strong>Susan:</strong> I’ll be nice. I promise you’ll be a{" "}
+          <mark>happy camper</mark>.
         </Text>
         <Text>
-          <strong>Bob:</strong> Okay. Let’s give it a shot, boss!
-        </Text>        
+          <strong>Bob:</strong> Okay. <mark>Let’s give it a shot</mark>, boss!
+        </Text>
       </div>
     </>
   );
