8a3f116a1f3db6465b17743ae198bd2e6d8173df
Paulo Silva <paulofernando78@gmail.com>
2026-01-09 19:36:41 -0300

    wip

--- src/content/students/extras/vocabulary/selaa/lesson-2.jsx
+++ src/content/students/extras/vocabulary/selaa/lesson-2.jsx
@@ -1,4 +1,5 @@
 import { Whiteboard } from "@/components/molecules/Whiteboard";
+import { DictionarySearch } from "@/components/molecules/DictionarySearch";
 import { Dialogue } from "@/components/molecules/Dialogue";
 import { Radio } from "@/components/molecules/Exercises/Radio";
 import { Ribbon } from "@/components/atoms/Ribbon";
@@ -13,6 +14,9 @@ export default function SELAAlessonTwo() {
         lesson="Lesson 2 • Bob Returns Home with Bad News"
       />
       <div className="line-break">
+        <div className="dictionary-area">
+          <DictionarySearch />
+        </div>
         <Dialogue
           title="Lesson 2 • Bob Returns Home with Bad News"
           audioPlayer="/assets/audio/vocabulary/selaa/lesson-2.mp3"
