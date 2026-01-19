aa24fb984016daec8392035f005fda9bf43717b0
Paulo Silva <paulofernando78@gmail.com>
2026-01-19 20:59:44 -0300

    wip

--- src/content/pronunciation/the-alphabet/index.jsx
+++ src/content/pronunciation/the-alphabet/index.jsx
@@ -59,7 +59,7 @@ export default function TheAlphabetPage() {
           ]}
         />
         <Note
-          items={[
+          lines={[
             {
               text: [
                 {
