02c210a450f5cf95288a2db8d973078a873177d8
Paulo Silva <paulofernando78@gmail.com>
2026-01-22 11:25:43 -0300

    wip

--- src/lib/svg-imports.js
+++ src/lib/svg-imports.js
@@ -148,14 +148,13 @@ export const Keyboard = () => (
   </svg>
 );
 
-export const Lesson = ({className, ...props}) => (
+export const Lesson = (props) => (
   <svg
   xmlns="http://www.w3.org/2000/svg"
   width={iconSize}
   height={iconSize}
   viewBox="0 -960 960 960"
   fill="#999999"
-  className={className}
   {...props}
   >
     <path d="M280-280h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Zm-80 480q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z" />
