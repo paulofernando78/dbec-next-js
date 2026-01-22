131100e23268725079d5136250cf443a8b42a25e
Paulo Silva <paulofernando78@gmail.com>
2026-01-22 03:54:28 -0300

    wip

--- src/content/reference/to-for.jsx
+++ src/content/reference/to-for.jsx
@@ -8,71 +8,54 @@ export default function toFOR() {
     <>
       <Whiteboard title="Vocabulary" subtitle="To vs. For" />
       <div className="line-break">
-        <Contents
-          items={[
-            { href: "introduction", label: "Introduction" },
-            { href: "presentation", label: "Presentation" },
-            { href: "meaning", label: "Meaning" },
-            { href: "practice", label: "Practice" },
-            { href: "production", label: "Production" },
+        <Paragraph
+          blocks={[
+            {
+              lines: [
+                {
+                  text: [
+                    {
+                      part: "to = opnion",
+                      type: "bold",
+                    },
+                  ],
+                },
+                {
+                  text: ["It seems important to me."],
+                },
+                {
+                  text: ["It looks attractive to me."],
+                },
+                {
+                  text: ["It feels bad to me."],
+                },
+                {
+                  lineBreak: true,
+                },
+                {
+                  text: [
+                    {
+                      part: "for = benefit",
+                      type: "bold",
+                    },
+                  ],
+                },
+                {
+                  text: ["Eating vegetables is good for me/everybody."],
+                },
+                {
+                  text: ["Practice regular exercise is good for me/everybody."],
+                },
+                {
+                  text: ["Smoking is bad for everybody."],
+                },
+                {
+                  text: ["This tool is useful for me."],
+                },
+              ],
+            },
           ]}
         />
-        <Section id="Introduction" label="Introduction" heading={3}></Section>
-        <Section id="Presentation" label="Presentation" heading={3}></Section>
-        <Section id="Meaning" label="Meaning" heading={3}>
-          <Paragraph
-            blocks={[
-              {
-                lines: [
-                  {
-                    text: [
-                      {
-                        part: "to = opnion",
-                        type: "bold",
-                      },
-                    ],
-                  },
-                  {
-                    text: ["It seems important to me."],
-                  },
-                  {
-                    text: ["It looks attractive to me."],
-                  },
-                  {
-                    text: ["It feels bad to me."],
-                  },
-                  {
-                    lineBreak: true,
-                  },
-                  {
-                    text: [
-                      {
-                        part: "for = benefit",
-                        type: "bold",
-                      },
-                    ],
-                  },
-                  {
-                    text: ["Eating vegetables is good for me/everybody."],
-                  },
-                  {
-                    text: [
-                      "Practice regular exercise is good for me/everybody.",
-                    ],
-                  },
-                  {
-                    text: ["Smoking is bad for everybody."],
-                  },
-                  {
-                    text: ["This tool is useful for me."],
-                  },
-                ],
-              },
-            ]}
-          />
-        </Section>
-        <Section id="Practice" label="Practice" heading={3}></Section>
-        <Section id="Production" label="Production" heading={3}></Section>
       </div>
     </>
   );
