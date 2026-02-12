bddf79774e34ce56b95f5ea5b4706b172fd19a45
Paulo Silva <paulofernando78@gmail.com>
2026-02-12 04:19:14 -0300

    wip

--- src/content/pronunciation/phonetics-letters.jsx
+++ src/content/pronunciation/phonetics-letters.jsx
@@ -6246,7 +6246,7 @@ export default function PhoneticsLetters() {
                 audio:
                   "/assets/audio/general/dont-forget-to-wash-your-hands.mp3",
                 englishExample: "Don’t forget to wash your hands.",
-                portugueseExample: "Eu lavo minhas mãos.",
+                portugueseExample: "Não esqueça de lavar as suas mãos.",
               }),
               example({
                 audio: "/assets/audio/general/i-usually-watch-tv-at-night.mp3",
