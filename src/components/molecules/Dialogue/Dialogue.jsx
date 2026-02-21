507171a47cee3237551f7d6659a149f77e2a8926
Paulo Silva <paulofernando78@gmail.com>
2026-02-20 22:18:46 -0300

    wip

--- src/components/molecules/Dialogue/Dialogue.jsx
+++ src/components/molecules/Dialogue/Dialogue.jsx
@@ -1,12 +1,14 @@
 import styles from "./Dialogue.module.css";
 import { Italic } from "@/components/atoms/Italic";
 import { Bold } from "@/components/atoms/Bold";
+import { AudioPlayer } from "@/components/atoms/AudioPlayer";
 import { ContentToken } from "@/components/molecules/ContentToken";
 
-export const Dialogue = ({ description, lines = [] }) => {
+export const Dialogue = ({ description, audioPlayer, lines = [] }) => {
   return (
     <div className="line-break">
       <p><Italic>{description}</Italic></p>
+      <AudioPlayer src={audioPlayer}/>
       {lines.map((line, index) => (
         <p key={index} className={styles.lines}>
           {line.speaker && <Bold>{line.speaker} </Bold>}
